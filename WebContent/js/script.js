ee27f69bf80a3e923a33c407929fae9af9485a7f
bedoyama <bedoyama@gmail.com>
2016-03-01 16:28:04 -0500

    full publishers list consuming restful

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,56 +1,26 @@
 // Code goes here
 
-var myApp = angular.module('myApp', ['angularUtils.directives.dirPagination']);
+var myApp = angular.module('myApp', [ 'angularUtils.directives.dirPagination',
+		'bookstoreServices' ]);
 
-function MyController($scope) {
+function MyController($scope, Publisher) {
+	$scope.currentPage = 1;
+	$scope.pageSize = 10;
 
-  $scope.currentPage = 1;
-  $scope.pageSize = 10;
-  $scope.meals = [];
+	 Publisher.query(function(data) {
+		 $scope.publishers = data;
+	 });
 
-  var dishes = [
-    'noodles',
-    'sausage',
-    'beans on toast',
-    'cheeseburger',
-    'battered mars bar',
-    'crisp butty',
-    'yorkshire pudding',
-    'wiener schnitzel',
-    'sauerkraut mit ei',
-    'salad',
-    'onion soup',
-    'bak choi',
-    'avacado maki'
-  ];
-  var sides = [
-    'with chips',
-    'a la king',
-    'drizzled with cheese sauce',
-    'with a side salad',
-    'on toast',
-    'with ketchup',
-    'on a bed of cabbage',
-    'wrapped in streaky bacon',
-    'on a stick with cheese',
-    'in pitta bread'
-  ];
-  for (var i = 1; i <= 100; i++) {
-    var dish = dishes[Math.floor(Math.random() * dishes.length)];
-    var side = sides[Math.floor(Math.random() * sides.length)];
-    $scope.meals.push('meal ' + i + ': ' + dish + ' ' + side);
-  }
-  
-  $scope.pageChangeHandler = function(num) {
-      console.log('meals page changed to ' + num);
-  };
+	$scope.pageChangeHandler = function(num) {
+		console.log('meals page changed to ' + num);
+	};
 }
 
 function OtherController($scope) {
-  $scope.pageChangeHandler = function(num) {
-    console.log('going to page ' + num);
-  };
+	$scope.pageChangeHandler = function(num) {
+		console.log('going to page ' + num);
+	};
 }
 
-myApp.controller('MyController', MyController);
-myApp.controller('OtherController', OtherController);
+myApp.controller('MyController', [ '$scope', 'Publisher', MyController ]);
+myApp.controller('OtherController', [ '$scope', OtherController ]);
