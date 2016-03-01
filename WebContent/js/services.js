6adb6d41e44a1dad931cda0f47357c6d44c57478
bedoyama <bedoyama@gmail.com>
2016-03-01 16:28:20 -0500

    full publishers list consuming restful

---
+++ WebContent/js/services.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* Services */
+
+var bookstoreServices = angular.module('bookstoreServices', ['ngResource']);
+
+bookstoreServices.factory('Publisher', ['$resource',
+  function($resource){
+    return $resource('/bookstore/publisher/:publisherId');
+  }]);
