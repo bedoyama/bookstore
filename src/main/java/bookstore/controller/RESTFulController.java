58fee19910606a891780df4e435eb69c89aba875
bedoyama <bedoyama@gmail.com>
2016-03-02 19:31:27 -0500

    updated RESTFul added create update pub

--- src/main/java/bookstore/controller/RESTFulController.java
+++ src/main/java/bookstore/controller/RESTFulController.java
@@ -3,14 +3,17 @@ package bookstore.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import bookstore.model.Publisher;
 import bookstore.service.PublisherService;
@@ -46,7 +49,7 @@ public class RESTFulController {
 	@RequestMapping(value = "/publisher", params = { "page", "size" }, method = RequestMethod.GET)
 	public ResponseEntity<List<Publisher>> listAllPublishersPaginated(@RequestParam("page") Integer page,
 			@RequestParam("size") Integer size) {
-		List<Publisher> publishers = publisherService.listAllPublishers(page,size);
+		List<Publisher> publishers = publisherService.listAllPublishers(page, size);
 		if (publishers.isEmpty()) {
 			return new ResponseEntity<List<Publisher>>(HttpStatus.NO_CONTENT);// You
 																				// many
@@ -70,6 +73,47 @@ public class RESTFulController {
 			return new ResponseEntity<Publisher>(HttpStatus.NOT_FOUND);
 		}
 		return new ResponseEntity<Publisher>(publisher, HttpStatus.OK);
+	}
+
+	// -------------------Create a
+	// Publisher--------------------------------------------------------
+
+	@RequestMapping(value = "/publisher/", method = RequestMethod.POST)
+	public ResponseEntity<Publisher> createPublisher(@RequestBody Publisher publisher, UriComponentsBuilder ucBuilder) {
+		System.out.println("Creating Publisher " + publisher.getPname());
+
+		// if (publisherService.isPublisherExist(publisher)) {
+		// System.out.println("A Publisher with name " + publisher.getName() +
+		// " already exist");
+		// return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+		// }
+
+		publisherService.savePublisher(publisher);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(ucBuilder.path("/publisher/{id}").buildAndExpand(publisher.getPublisherId()).toUri());
+		return new ResponseEntity<Publisher>(publisher, HttpStatus.CREATED);
+	}
+
+	// ------------------- Update a Publisher
+	// --------------------------------------------------------
+
+	@RequestMapping(value = "/publisher/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Publisher> updatePublisher(@PathVariable("id") Long id, @RequestBody Publisher publisher) {
+		System.out.println("Updating Publisher " + id);
+
+		Publisher currentPublisher = publisherService.findPublisherById(id);
+
+		if (currentPublisher == null) {
+			System.out.println("Publisher with id " + id + " not found");
+			return new ResponseEntity<Publisher>(HttpStatus.NOT_FOUND);
+		}
+
+		currentPublisher.setPname(publisher.getPname());
+		currentPublisher.setPemail(publisher.getPemail());
+		
+		publisherService.updatePublisher(currentPublisher);
+		return new ResponseEntity<Publisher>(currentPublisher, HttpStatus.OK);
 	}
 
 }
